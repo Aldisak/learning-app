78a3c22a828669e1961bf6e45b05d0f4ccaddfc3
Aleš Matějka <ama@cca,cz>
2022-11-28 21:49:46 +0100

    Flipping card

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -1,16 +1,17 @@
 import {useEffect, useState} from 'react';
+import {Card, CardProps} from "./card";
 
 export function CardList() {
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState<CardProps[]>([]);
 
     useEffect(() => {
-        fetch("/api/cards")
+        fetch("https://retoolapi.dev/0xBBjN/learningApp")
             .then((response) => response.json())
             .then((cards) => setCards(cards));
     }, []);
 
     return (
-        <div className="card-list">
+        <div className="row row-cols-1 row-cols-md-4 g-4">
             {cards.map((card, index) => (
                 <Card key={index} {...card} />
             ))}
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,89 @@
-interface CardProps {
+import {useState} from "react";
+import styled from "styled-components";
+
+export interface CardProps {
     question: string;
     answer: string;
 }
 
-function Card({answer, question}: CardProps) {
+export function Card({answer, question}: CardProps) {
+    const [showAnswer, setShowAnswer] = useState(false);
+
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{question}</h5>
-                <p className="card-text">{answer}</p>
-            </div>
-        </div>
+        <CardContainer>
+            <CardBody className={showAnswer ? "is-flipped" : ""}>
+                <CardFace className="card__face--front">
+                    <CardTitle>{question}?</CardTitle>
+                    <CardButton onClick={() => setShowAnswer(!showAnswer)}>
+                        Show Answer
+                    </CardButton>
+                </CardFace>
+                <CardFace className="card__face--back">
+                    <CardTitle>{answer}.</CardTitle>
+                    <CardButton onClick={() => setShowAnswer(!showAnswer)}>
+                        Show Question
+                    </CardButton>
+                </CardFace>
+            </CardBody>
+        </CardContainer>
     );
 }
+
+const CardContainer = styled.div`
+  height: 260px;
+  perspective: 600px;
+`;
+
+const CardBody = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transform-origin: center right;
+  transition: transform 0.5s;
+
+  &.is-flipped {
+    transform: translateX(-100%) rotateY(-180deg);
+  }
+`;
+
+const CardFace = styled.div`
+  position: absolute;
+  border: 1px solid #eee;
+  width: 100%;
+  height: 100%;
+  color: black;
+  text-align: center;
+  font-weight: bold;
+  font-size: 40px;
+  backface-visibility: hidden;
+
+  &.card__face--back {
+    transform: rotateY(180deg);
+  }
+`;
+
+const CardTitle = styled.h5`
+    font-size: 24px;
+    font-weight: bold;
+    margin-top: 64px;
+`;
+
+const CardButton = styled.button`
+    position: absolute;
+    width: 80%;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 8px 16px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    background-color: #eee;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+  
+    &:hover {
+        background-color: #ddd;
+    }
+`;
